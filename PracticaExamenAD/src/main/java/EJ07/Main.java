dc17db13b7b927ebc79af7bff3a2e20aa0ac7eb3
HugoD24 <4048314@alu.murciaeduca.es>
2025-10-27 12:41:30 +0100

    añadido2

--- PracticaExamenAD/src/main/java/EJ07/Main.java
+++ PracticaExamenAD/src/main/java/EJ07/Main.java
@@ -22,11 +22,14 @@ ordenados alfabéticamente por apellidos y nombre.*/
 import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     static void main() {
 
+        Scanner sc = new Scanner(System.in);
         ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+        int seleccion;
 
         Alumno alumno1 = new Alumno("49823966D", "Hugo", "Desiderio Garcia", "638523367", LocalDate.of(2003, 02, 24), true);
         listaAlumnos.add(alumno1);
@@ -34,6 +37,44 @@ public class Main {
         listaAlumnos.add(alumno2);
         Alumno alumno3 = new Alumno("47235766R","Pepe", "Garcia Romero", "617250258", LocalDate.of(2001,11,10),false);
         listaAlumnos.add(alumno3);
+
+    do {
+        System.out.println("Bienvenido al sistema de gestion de alumnos");
+        System.out.println("Que acción deseas realizar: ");
+        System.out.println("0 - Finalizar app");
+        System.out.println("1 - Alta alumno");
+        System.out.println("2 - Eliminar alumno");
+        System.out.println("3 - Modificar alumno");
+        System.out.println("4 - Listar alumnos");
+
+        System.out.println("Selecciona una opcion");
+        seleccion = sc.nextInt();
+
+            switch (seleccion) {
+                case 0:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                case 1:
+                    System.out.println("Has seleccionado: Alta alumno");
+                    break;
+                case 2:
+                    System.out.println("Has seleccionado: Eliminar alumno");
+                    break;
+                case 3:
+                    System.out.println("Has seleccionado: Modificar alumno");
+                    break;
+                case 4:
+                    System.out.println("Has seleccionado: Listar alumnos");
+                    break;
+                default:
+                    System.out.println("Opción no válida, intenta de nuevo.");
+
+            }
+
+        }  while (seleccion != 0);
+
+
+
 
         File fichero = new File("ejercicioPractica.dat");
 
